dafb0c47cd42834643d0d713fdf669ed60be4aae
Arsen <117418895+aokidze@users.noreply.github.com>
2024-06-02 08:37:51 +0300

    Add files via upload

--- Хакатон/script.js
+++ Хакатон/script.js
@@ -2,17 +2,19 @@ let modal = document.getElementById("myModal");
 let btn = document.getElementById("openModal");
 let span = document.getElementsByClassName("close")[0];
 document.getElementById("loader").style.display = "none";
+document.getElementById("table-res").style.display = "none";
+document.querySelector(".Block_download").style.display = "none";
+document.getElementById("xar-res").style.display = "none";
+document.getElementById("report").style.display = "none";
 
 function closeBlocks() {
-  
   modal.style.display = "none";
   document.getElementById("table-res").style.display = "none";
   document.querySelector(".Block_download").style.display = "none";
   document.getElementById("xar-res").style.display = "none";
   document.getElementById("report").style.display = "none";
   document.getElementById("loader").style.display = "block";
-  setTimeout(function() {
-
+  setTimeout(function () {
     document.getElementById("table-res").style.display = "block";
     document.querySelector(".Block_download").style.display = "block";
     document.getElementById("xar-res").style.display = "block";
@@ -21,21 +23,20 @@ function closeBlocks() {
   }, 5000);
 }
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     closeBlocks();
   }
-}
+};
 
-
-document.getElementById("file-upload").addEventListener("change", function() {
+document.getElementById("file-upload").addEventListener("change", function () {
   closeBlocks();
 });
